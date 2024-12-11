5d6b3ca28f05f4e3df78d55509d3e8e9c2f82e4d
GGavino <gabriel.gavina07@gmail.com>
2024-12-11 16:52:59 +0000

    Refactor GetAllFiles view to return specific file fields in response

--- rest_api_server/api/views/files.py
+++ rest_api_server/api/views/files.py
@@ -3,11 +3,18 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.db import connection
 
-
+        
 class GetAllFiles(APIView):
     def get(self, request):
         with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM files")
+            cursor.execute("SELECT id, csv_file_path, xml_file_path FROM files")
             result = cursor.fetchall()
-        files = [Book(id=row[0], name=row[1]) for row in result]
+            files = [
+                {
+                    "id": row[0],
+                    "csv_file_path": row[1],
+                    "xml_file_path": row[2]
+                }
+                for row in result
+            ]
         return Response({"files": files}, status=status.HTTP_200_OK)
