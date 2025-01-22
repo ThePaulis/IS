88daa4b89a3d5e8129c37b6f0889c02a07246239
John Doe <john.doe@example.com>
2025-01-22 17:01:15 +0000

    a

--- graphql_api/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PlayersView, PlayersByCountryView
-
-urlpatterns = [
-    path('players/', PlayersView.as_view(), name='warehouses_list'),
-    path('playersByCountry/', PlayersByCountryView.as_view(), name='warehouses_list'),
-
-]
